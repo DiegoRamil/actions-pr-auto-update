0f24b785923835c2bc38a28f8560410c59ae6012
DiegoRamil <nxquumodding@gmail.com>
2024-12-18 13:35:07 +0100

    style(function): remove console log

--- src/status.ts
+++ src/status.ts
@@ -2,7 +2,6 @@ let VALID_STATUS: number[] = []
 
 export const obtainValidStatus = (): number[] => {
   if (VALID_STATUS.length === 0) {
-    console.log('Creating the array with valid status codes')
     VALID_STATUS = HTTP_OK_STATUS
   }
   return VALID_STATUS
